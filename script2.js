cde77eba5626f452730a417d29c0a73943a45b21
yangchuangwun <127645403+yangchuangwun@users.noreply.github.com>
2023-04-04 18:52:09 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+const wasuretatoroku = document.getElementById("wasuretatoroku");
+
+
+
+// 忘記密碼頁面的登入
+wasuretatoroku.addEventListener("click", () => {
+    window.location.href = ("Steam.html")
+});
